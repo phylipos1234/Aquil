3f8fb27aaa4b5f31e471a2ae72606d5362e05577
phylipos1234 <phylipostesfaye105@gmail.com>
2025-06-09 13:42:39 +0300

    .

--- src/pages/professinals/components/ProfessinalsMain.jsx
+++ src/pages/professinals/components/ProfessinalsMain.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import d from '/images/professinals/d.png'
+import v from '/images/professinals/v.png'
+import e from '/images/professinals/e.png'
+
 
 const ProfessinalsMain = () => {
   return (
-    <div>ProfessinalsMain</div>
+    <div className='flex flex-col py-6' >
+
+        {/* First Image row */}
+        <div className='flex flex-row w-full justify-between px-1' >
+        <img src={d} alt="imag-1" className="" />
+        <img src={v} alt="imag-1" className="" />
+        <img src={e} alt="imag-1" className="" />
+       
+
+        </div>
+        
+    </div>
   )
 }
 
--- src/pages/professinals/index.jsx
+++ src/pages/professinals/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import ProfessinalsMain from './components/ProfessinalsMain'
 
 const ProfessionalsPage = () => {
   return (
-    <div className=" min-h-screen py-14 flex flex-col justify-between px-6 sm:px-16">
+    <div className=" min-h-screen py-8 flex flex-col justify-between ">
         <Header textColor='text-[#000]' />
+        <ProfessinalsMain />
+
         {/* Section Title and Content */}
         {/* <Footer/> */}
     </div>

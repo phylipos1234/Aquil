c76db6048f4c98c125e41f7e98b83c54f1b4757e
phylipos1234 <phylipostesfaye105@gmail.com>
2025-06-09 17:57:07 +0300

    authenticity done

--- src/pages/professinals/components/Authenticity.jsx
+++ src/pages/professinals/components/Authenticity.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import authenticity from '/images/professinals/auth.png' 
 import boutique from '@/assets/icons/professionals/boutique.svg'
+import experience from '@/assets/icons/professionals/experience.svg'
+import emerging from '@/assets/icons/professionals/emerging.svg'
 
 const Authenticity = () => {
   return (
@@ -15,25 +17,25 @@ const Authenticity = () => {
             <h1 className="text-[36px] text-[#0A0024] leading-[44px] font-berlingske font-semibold">
              Premium Experiences
             </h1>
-            <p className='text-[20px] text-[#0A0024] leading-[28px]  font-berlingske  tracking-[1%]  mt-10 '>
-               Thoughtfully crafted journeys and premium business travel services designed specifically for your discerning clients. Aquila provides exclusive travel benefits, tailored itineraries, and meticulous execution, ensuring every journey is exceptional and effortless, reflecting your agency’s high standards.
+            <p className='text-[20px] text-[#0A0024] leading-[28px]  font-berlingske  tracking-[1%]  mt-10 w-[70%] '>
+            Gain direct access travel to a carefully curated portfolio of unique and luxurious experiences, accommodations, and destinations. Each offering is expertly chosen to resonate deeply with sophisticated travelers seeking exclusivity, authenticity, and refined elegance.
                </p>
 
 
             {/* Lists */}
-               <div className='flex flex-col gap-4 mt-6'>
+               <div className='flex flex-col gap-6 mt-8 '>
                 <div className='flex flex-row gap-4 items-center'>
                     <img src={boutique} alt="boutique" className="" />
-                <p className="text-[#0A0024] text-[12px]  leading-[19px] font-neutra  tracking-[1.56px]">AUTHENTICITY </p>
+                <p className="text-[#0A0024] text-[15px]  leading-[19px] font-neutra  tracking-[1.56px] uppercase ">Boutique partnership </p>
                     </div>
 
                     <div className='flex flex-row gap-4 items-center'>
-                    <img src={boutique} alt="boutique" className="" />
-                <p className="text-[#0A0024] text-[12px]  leading-[19px] font-neutra  tracking-[1.56px]">AUTHENTICITY </p>
+                    <img src={experience} alt="boutique" className="" />
+                <p className="text-[#0A0024] text-[15px]  leading-[19px] font-neutra  tracking-[1.56px] uppercase ">Authentic experiences</p>
                     </div>
                     <div className='flex flex-row gap-4 items-center'>
-                    <img src={boutique} alt="boutique" className="" />
-                <p className="text-[#0A0024] text-[12px]  leading-[19px] font-neutra  tracking-[1.56px]">AUTHENTICITY </p>
+                    <img src={emerging} alt="boutique" className="" />
+                <p className="text-[#0A0024] text-[15px]  leading-[19px] font-neutra  tracking-[1.56px] uppercase">emerging destinations</p>
                     </div>
                </div>
             
